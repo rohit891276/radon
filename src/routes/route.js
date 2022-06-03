f957d521be5adb93c8bc2b6bf38a7dd9b6b1dadf
rohti891276 <prohit.kumar72@gmail.com>
2022-06-03 08:37:36 +0530

    previous assignment updated

--- src/routes/route.js
+++ src/routes/route.js
@@ -61,19 +61,6 @@ router.get('/hello', function (req, res) {
 });
 
 
-/* app.get("/sol1", function (req, res) {
-    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
-    let arr= [1,2,3,5,6,7]
-    let sum = sum + arr[i]
-    let n = arr.length
-    let missingNumber = getMissingNo(arr, n)
-    console.log(missingNumber)
-
-    ///LOGIC WILL GO HERE 
-    res.send(  { data: missingNumber  }  );
-}); */
-
-
 
 module.exports = router;
 
