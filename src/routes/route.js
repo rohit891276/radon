08af086cebbf0e5b22c5c967134251dc9eab4a25
rohti891276 <prohit.kumar72@gmail.com>
2022-06-06 20:21:23 +0530

    Day-2

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,7 +5,7 @@ const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 
 router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
+    res.send("My first ever api!!")
 })
 
 router.post("/createUser", UserController.createUser  )
