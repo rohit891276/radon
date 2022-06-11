3e6e1ca5107e450a8d3892daf1161469622082e7
rohti891276 <prohit.kumar72@gmail.com>
2022-06-12 00:00:54 +0530

    middleware assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,46 +1,6 @@
-const UserModel= require("../models/userModel")
-
-
-
-
 const basicCode= async function(req, res) {
-    let tokenDataInHeaders= req.headers.token
-    console.log(tokenDataInHeaders)
 
-    console.log( "HEADER DATA ABOVE")
-    console.log( "hey man, congrats you have reached the Handler")
-    res.send({ msg: "This is coming from controller (handler)"})
+    res.send({ msg: "Hey I have successfully completed the middleware test"})
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
-}
-
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
-}
-
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
 module.exports.basicCode= basicCode
--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,26 +1,10 @@
+const moment = require('moment')
 
-const mid1= function ( req, res, next) {
-    req.falana= "hi there. i am adding something new to the req object"
-    console.log("Hi I am a middleware named Mid1")
+
+const ctipData = async function(req, res,next){
+    let timestamp = moment()
+    console.log(timestamp.format(" YYYY-MM-DD HH-MM-SS")+" "+ req.ip+" "+ req.path)
     next()
 }
 
-const mid2= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid2")
-    next()
-}
-
-const mid3= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid3")
-    next()
-}
-
-const mid4= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid4")
-    next()
-}
-
-module.exports.mid1= mid1
-module.exports.mid2= mid2
-module.exports.mid3= mid3
-module.exports.mid4= mid4
+module.exports.ctipData = ctipData
