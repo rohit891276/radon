a65ef32b501df17de12559ee52d55ec7213b35a0
rohti891276 <prohit.kumar72@gmail.com>
2022-06-28 21:08:30 +0530

    Project-1/Blogging Site

--- src/index.js
+++ src/index.js
@@ -14,13 +14,6 @@ mongoose.connect("mongodb+srv://harsh-developer:aA12345678@cluster0.lxbes.mongod
 .catch ( err => console.log(err) )
 
 
-mongoose.connect('mongodb+srv://harsh-developer:aA12345678@cluster0.lxbes.mongodb.net/Project-blogs?retryWrites=true&w=majority',{
-      useNewUrlParser: true,
-    }
-  )
-  .then(() => console.log('MongoDb is connected'))
-  .catch((err) => console.log(err));
-
 app.use('/', route);
 
 app.all('/**', (req, res) => {
